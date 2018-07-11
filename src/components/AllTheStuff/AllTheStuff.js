8a7a824b6ff670b172386310110f76201f8ed230
Amanda Mitchell <wreedamitchell@gmail.com>
2018-07-11 18:17:26 -0500

    added bootstrap and components that I thought I would need for project

---
+++ src/components/AllTheStuff/AllTheStuff.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './AllTheStuff.css';
+
+class AllTheStuff extends React.Components {
+  render () {
+    return (
+      <div className="AllTheStuff">AllTheStuff</div>
+    );
+  }
+}
+
+export default AllTheStuff;
---
+++ src/components/Items/Items.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Items.css';
+
+class Items extends React.Components {
+  render () {
+    return (
+      <div className="Items">Items</div>
+    );
+  }
+}
+
+export default Items;
