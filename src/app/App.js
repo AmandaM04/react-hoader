a9f4a27dd53a18b634b9b4237f9e3db0919a47fb
Amanda Mitchell <wreedamitchell@gmail.com>
2018-07-11 19:39:27 -0500

    items componet setup

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 
 import './App.css';
+
+import AllTheStuff from '../components/AllTheStuff/AllTheStuff';
+import Login from '../components/Login/Login';
+import MyStuff from '../components/MyStuff/MyStuff';
+import Navbar from '../components/Navbar/Navbar';
+import Register from '../components/Register/Register';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <div>
+            <Navbar
+              authed={this.state.authed}
+              runAway={this.runAway}
+            />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Login} />
+                  <PrivateRoute
+                    path="/allthestuff"
+                    authed={this.state.authed}
+                    component={AllTheStuff} />
+                  <PublicRoute
+                    path="/register"
+                    authed={this.state.authed}
+                    component={Register} />
+                  <PublicRoute
+                    path="/login"
+                    authed={this.state.authed}
+                    component={Login} />
+                  <PrivateRoute
+                    path="/mystuff"
+                    authed={this.state.authed}
+                    component={MyStuff} />
+                  <PrivateRoute
+                    path="/order/:id"
+                    authed={this.state.authed}
+                    component={SingleItem} />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
